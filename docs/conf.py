406c7bf2e5b755572efdcfb5a245147f320b3f36
Albert Brouwer <brouwer@iiasa.ac.at>
2021-09-30 14:03:23 +0200

    Tweak sidebar background color via custom CSS

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# -- Setup for custom CSS-----------------------------------------------------
 
+def setup(app):
+    app.add_css_file('custom.css')
+ 
 # -- Project information -----------------------------------------------------
 
 project = 'COACCH Data Repository'
