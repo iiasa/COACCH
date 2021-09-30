cdb72313eb5815917592b1c1147d6cf75d5564b9
Albert Brouwer <brouwer@iiasa.ac.at>
2021-09-30 13:51:06 +0200

    Add a COACCH sidebar logo

--- docs/conf.py
+++ docs/conf.py
@@ -83,12 +83,20 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# If given, this must be the name of an image file that is the logo of the docs.
+# It is placed at the top of the sidebar; its width should therefore not exceed
+# 200 pixels. Default: None.
+html_logo = '_static/3-LOGO-COACCH_horizontal-transparent.png'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
