f7e947bc23ba946213f6295c1304509868712fb1
hvalin <valin.hugo@gmail.com>
2021-10-01 16:32:05 +0200

    Modifying copyright

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'COACCH Data Repository'
-copyright = '2021, Esther Boere'
-author = 'Esther Boere'
+copyright = '2021, IIASA'
+author = 'IIASA'
 
 # The short X.Y version
 version = ''
@@ -203,7 +203,7 @@ if 'REPO_NAME' in os.environ:
 	REPO_NAME = os.environ['REPO_NAME']
 else:
 	REPO_NAME = ''
- 
+
 # SET CURRENT_LANGUAGE
 if 'current_language' in os.environ:
    # get the current_language env var set by buildDocs.sh
@@ -212,14 +212,14 @@ else:
    # the user is probably doing `make html`
    # set this build's current language to english
    current_language = 'en'
- 
+
 # tell the theme which language to we're currently building
 html_context['current_language'] = current_language
- 
+
 # SET CURRENT_VERSION
 from git import Repo
 repo = Repo( search_parent_directories=True )
- 
+
 if 'current_version' in os.environ:
    # get the current_version env var set by buildDocs.sh
    current_version = os.environ['current_version']
@@ -227,28 +227,28 @@ else:
    # the user is probably doing `make html`
    # set this build's current version by looking at the branch
    current_version = repo.active_branch.name
- 
+
 # tell the theme which version we're currently on ('current_version' affects
 # the lower-left rtd menu and 'version' affects the logo-area version)
 html_context['current_version'] = current_version
 html_context['version'] = current_version
- 
+
 # POPULATE LINKS TO OTHER LANGUAGES
 html_context['languages'] = [ ('en', '/' +REPO_NAME+ '/en/' +current_version+ '/') ]
- 
+
 languages = [lang.name for lang in os.scandir('locales') if lang.is_dir()]
 for lang in languages:
    html_context['languages'].append( (lang, '/' +REPO_NAME+ '/' +lang+ '/' +current_version+ '/') )
- 
+
 # POPULATE LINKS TO OTHER VERSIONS
 html_context['versions'] = list()
- 
+
 versions = [branch.name for branch in repo.branches]
 for version in versions:
    html_context['versions'].append( (version, '/' +REPO_NAME+ '/'  +current_language+ '/' +version+ '/') )
- 
+
 # POPULATE LINKS TO OTHER FORMATS/DOWNLOADS
- 
+
 # settings for creating PDF with rinoh
 rinoh_documents = [(
  master_doc,
@@ -257,21 +257,21 @@ rinoh_documents = [(
  '© ' +copyright,
 )]
 today_fmt = "%B %d, %Y"
- 
+
 # settings for EPUB
 epub_basename = 'target'
- 
+
 html_context['downloads'] = list()
 html_context['downloads'].append( ('pdf', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version+ '/' +project+ '-docs_' +current_language+ '_' +current_version+ '.pdf') )
- 
+
 html_context['downloads'].append( ('epub', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version+ '/' +project+ '-docs_' +current_language+ '_' +current_version+ '.epub') )
- 
+
 ##########################
 # "EDIT ON GITHUB" LINKS #
 ##########################
- 
+
 html_context['display_github'] = True
 html_context['github_user'] = 'iiasa'
 html_context['github_repo'] = 'COACCH'
 html_context['github_version'] = 'master/docs/'
- 
+
